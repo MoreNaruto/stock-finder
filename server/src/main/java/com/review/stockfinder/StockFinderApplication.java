090fb02392e59787925b95ace6c9dcd6d4d75ae4
Thomas Morris <morenaruto@Thomass-MacBook-Pro.local>
2020-03-13 16:21:39 -0500

    Set up spring app

---
+++ server/src/main/java/com/review/stockfinder/StockFinderApplication.java
@@ -0,0 +1,13 @@
+package com.review.stockfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockFinderApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/review/stockfinder/StockFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.review.stockfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
